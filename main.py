e8bc8ef4571b8853e72f0fb28f53178d67cf6774
Timenem <m19860203@mail.com>
2023-07-07 14:28:19 +0300

    main methods

---
+++ main.py
@@ -0,0 +1,31 @@
+
+class AvitoParser(object):
+    def __init__(self, url: str, items: list, pages_count: int = 100, browser_version=None):
+        """init method"""
+        self.url = url
+        self.items = items
+        self.pages_count = pages_count
+        self.browser_version = browser_version
+        self.data = []
+
+    def __set_config(self):
+        """ private method
+        run browser"""
+
+    def __get_url(self):
+        """ get page"""
+
+    def __paginator(self):
+        """ private method
+        click next page"""
+
+    def __parse_page(self):
+        """ private method
+         parse one page"""
+
+    def save_data(self):
+        """ save data """
+    def parse(self):
+        """ main method"""
+
+
