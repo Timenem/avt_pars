6651320c5b722a421ca4803f4aea1418cfd01bb6
Timenem <56394536+Timenem@users.noreply.github.com>
2023-07-08 09:46:01 +0300

    Update main.py
    added methods

--- main.py
+++ main.py
@@ -15,20 +15,39 @@ class AvitoParser(object):
         options.add_argument('--headless')
         self.driver = uc.Chrome(browser_version=self.browser_version, options=options)
 
+   
     def __get_url(self):
-        """ get page"""
+        """get page"""
+        self.driver.get(self.url)
 
     def __paginator(self):
         """ private method
         click next page"""
+        while self.driver.find_elements(By.CSS_SELECTOR,
+                                        "[data-marker='pagination-button/next']") and self.pages_count > 0:
+            self.__parse_page()
+            self.driver.find_element("[data-marker='pagination-button/next']").click()
+            self.pages_count -= 1
 
     def __parse_page(self):
         """ private method
          parse one page"""
+        titles = self.driver.find_elements(By.CSS_SELECTOR, "[data-marker='item']")
+        for title in titles:
+            name = title.find_element(By.CSS_SELECTOR, "[itemprop='name']").text
+            description = title.find_element(By.CSS_SELECTOR, "[class*='item-description']")
+            url = title.find_element(By.CSS_SELECTOR, "[data-marker='item-title']").get_attribute('href')
+            price = title.find_element(By.CSS_SELECTOR, "[itemprop='price']").get_attribute('content')
+            data = {'name': name, 'description': description, 'url': url, 'price': price}
+            if any([item.lower() in description for item in self.items]) and int(price) == 0:
+                self.data.append(data)
+        self.save_data()
 
     def save_data(self):
         """ save data """
+        pass 
     def parse(self):
         """ main method"""
 
-
+if __name__ == '__main__':
+    AvitoParser()
